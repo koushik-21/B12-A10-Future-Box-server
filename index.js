79efd507941c65d2084773a70743e152bcc71eff
koushik-21 <koushik.biswas.edu@gmail.com>
2025-11-11 00:25:25 +0600

    products-api-built

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-//middle-ware -importExportHub-qjQtwdWtbgIBomiE
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>middle-ware-connection<<<<<<<<<<<<<<<<<<<<<<<<
 app.use(cors());
 app.use(express.json());
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>DATABASE_CONNECTION_MONGODB_<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hc5ykgf.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
