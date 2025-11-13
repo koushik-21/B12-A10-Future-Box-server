2e42d795a20c2d9ee38cff1d4ec7b390d0948c8e
koushik-21 <koushik.biswas.edu@gmail.com>
2025-11-13 13:33:54 +0600

    redeploying

--- index.js
+++ index.js
@@ -229,7 +229,9 @@ async function run() {
     //   res.send(result);
     // });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
+    
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
