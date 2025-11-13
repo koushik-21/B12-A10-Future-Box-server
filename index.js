668a64626a12dc49b0f3e945b8e40b0e268511b8
koushik-21 <koushik.biswas.edu@gmail.com>
2025-11-13 16:27:20 +0600

    latest-push

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("importExportBD");
     const productsCollection = db.collection("products");
@@ -230,7 +230,6 @@ async function run() {
     // });
     // Send a ping to confirm a successful connection
 
-    
     // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
